8d43756ca58076d6225335601ad0180d0d6022c7
mcappato <mcappato@makingsense.com>
2023-02-15 10:26:47 -0300

    open popup external url

--- pushservice.js
+++ pushservice.js
@@ -1,1 +1,7 @@
-alert("notificaciones push");
+function popup(url,windowName) {
+  newWindow=window.open(url,windowName,'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=600, height=250, top=100, left=100');
+  if (window.focus) {newWindow.focus()}
+  return false;
+}
+
+popup("https://news-push.netlify.app/","notificaciones Cliente1");
